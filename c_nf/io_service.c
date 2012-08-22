8da0701f74cbe2934e05676c13be8e4954d66f8b
ZHOU Xiaobo <zhouxiaobo@zhou.local>
2012-08-22 13:53:23 +0800

    7

---
+++ c_nf/io_service.c
@@ -0,0 +1,108 @@
+#include "io_service.h"
+
+static struct socket_info* get_idle_client_socket_info(struct io_service *io, char *ipstr, uint16_t host_port)
+{
+  
+  struct socket_info *map[] = io->client_map;
+
+  assert(map);
+  struct in_addr ipaddr;
+  if (inet_aton(ipstr, &ipaddr) != 1) {
+    return 0;
+  }
+
+  struct socket_info *ret = 0;
+  uint16_t hash_code = *((uint16_t*)(((char*)&ipaddr.s_addr) + 2));
+  if (map[hash_code]) {
+    ret = map[hash_code];
+    if (map[hash_code]->next) {
+      map[hash_code]->next->prev = 0;
+      map[hash_code] = map[hash_code]->next;
+    } else {
+      map[hash_code] = 0;
+    }
+  } else {
+    ret = 0;
+  }
+  return ret;
+}
+
+static void return_client_socket_info(struct io_service *io, struct socket_info *info)
+{
+
+  struct socket_info *map[] = io->client_map;
+  assert(map);
+
+  struct timeval now;
+  gettimeofday(&now, 0);
+  info->atime_sec = now.tv_sec;
+  info->atime_usec = now.tv_usec;
+
+  uint16_t hash_code = *((uint16_t*)(((char*)&info->net_ip) + 2));
+  if (map[hash_code]) {
+    info->next = map[hash_code];
+    info->prev = 0;
+    map[hash_code]->prev = info;
+  } else {
+    info->next = info->prev = 0;
+    map[hash_code] = info;
+  }
+
+}
+
+static void mark_server_socket_info(struct io_service *io, strstruct socket_info *info)
+{
+  assert(io);
+  assert(info);
+
+  struct timeval now;
+  gettimeofday(&now, 0);
+  info->atime_sec = now.tv_sec;
+  info->atime_usec = now.tv_usec;
+
+  if (!info->prev) {
+    assert(io->server_list_head == info);
+  } else {
+
+    if (io->server_list_tail == info) {
+      io->server_list_tail = info->prev;
+    }
+    info->prev->next = info->next;
+
+    info->prev = 0;
+    info->next = io->server_list_head;
+    io->server_list_head = info;
+  }
+}
+
+static int destroy_idle_server_socket_info(struct io_service *io)
+{
+  struct socket_info *iter, *old;
+  
+  struct timeval now;
+  gettimeofday(&now, 0);
+  int ret = 0;
+  iter = io->server_list_tail;
+  while (iter) {
+    if ((now.tv_sec - iter->atime_sec) * 1000000 + now.tv_usec - iter->atime_usec < io->idle_timeout) {
+      break;
+    }
+    old = iter;
+    destroy_socket_info(iter);
+    ret++;
+    iter = old->prev;
+  }
+  if (iter) {
+    io->server_list_tail = iter;
+  } else {
+    io->server_list_head = io->server_list_tail = 0;
+  }
+
+  iter = old;
+  while (iter) {
+    old = iter;
+    iter = iter->next;
+    destroy_socket_info(old);
+  }
+  return ret;
+}
---
+++ c_nf/process.c
@@ -0,0 +1,265 @@
+#include "process.h"
+
+int process_recv_other_format(struct socket_info *info)
+{
+}
+
+/**
+   return value:
+   0: complete
+   1: continue
+   -1: packet length invalid
+   -2: error happened
+   -3: closed by peer
+ */
+int process_recv_lv_format(struct socket_info *info)
+{
+  assert(info);
+  assert(info->state & S_IN_PROCESS);
+  assert(info->state == S_IN_RECV);
+  assert(info->type == T_TCP_SERVER_LV || info->type == T_TCP_CLIENT_LV);
+
+  char *buf;
+  uint32_t buf_len;
+  uint32_t packet_len;
+
+  if (info->packet_len) {
+    info->packet_len = info->is_message_callback(info->socket, info->recv_buf, info->recv_buf_used);
+  }
+
+  if (info->packet_len > info->recv_buf_len_max) {
+    return -1;
+  } else if (info->packet_len > info->recv_buf_len) {
+    /* realloc a larger buffer */
+    char *old = info->recv_buf;
+    info->recv_buf_len = info->recv_buf_len_max;
+    info->recv_buf = (char*)malloc(info->recv_buf_len);
+    memcpy(info->recv_buf, old, info->recv_buf_used);
+    free(old);
+  } else {
+
+  }
+   
+  buf = info->recv_buf + info->recv_buf_used;
+  buf_len = info->packet_len - (info->recv_buf_len - info->recv_buf_used + 1);
+
+  /* begin to recv */
+  int cnt = 0;
+  uint32_t cur = 0;
+  uint32_t bytes_read = 0;
+
+  while ((cnt = recv(info->socket, buf + cur, buf_len - bytes_read, 0)) > 0) {
+    cur += cnt;
+    bytes_read -= cnt;
+    if (cur == buf_len) {
+      break;
+    }
+  }
+  info->recv_buf_used += cur;
+
+  int ret = 0;
+  if (cnt < 0) {
+    if (errno == EAGAIN) {
+      if (info->packet_len != 0 && info->packet_len == info->recv_buf_used) {
+        ret = 0;
+      } else {
+        ret = 1;
+      }
+    } else {
+      ret = -2;
+    }
+    
+  } else if (cnt == 0) {
+    ret = -3;
+  } else {
+    
+  }
+
+  return ret;
+}
+
+/**
+   return value:
+   0: complete
+   1: continue
+   -1: error happened
+   -2: closed by peer
+ */
+int process_tcp_send(struct socket_info *info)
+{
+  assert(info);
+  assert(info->state & S_IN_PROCESS);
+  assert(info->state == S_IN_SEND);
+  assert(info->type == T_TCP_SERVER_LV || info->type == T_TCP_CLIENT_LV);
+
+  char *buf;
+  uint32_t buf_len;
+
+  buf = info->send_buf + info->send_buf_sent;
+  buf_len = info->send_buf_len - info->send_buf_sent;
+
+  /* begin to send */
+  int cnt = 0;
+  uint32_t cur = 0;
+  uint32_t bytes_sent = 0;
+
+  while ((cnt = send(info->socket, buf + cur, buf_len - bytes_sent, 0)) > 0) {
+    cur += cnt;
+    bytes_sent -= cnt;
+    if (cur == buf_len) {
+      break;
+    }
+  }
+  info->send_buf_sent += cur;
+
+  int ret = 1;
+  if (cnt < 0) {
+    if (errno == EAGAIN) {
+      ret = 1;
+    } else {
+      ret = -1;
+    }
+  } else if (cnt == 0) {
+    ret = -2;
+  } else {
+    ret = 0;
+  }
+
+  return ret;
+}
+
+int process_udp_send(struct socket_info *info)
+{
+}
+
+int process_recv_http_format(struct socket_info *info)
+{
+}
+
+int process_recv_udp(struct socket_info *info)
+{
+}
+
+int process_socket_event(struct socket_info *info)
+{
+  assert(info);
+  
+  int ret = 0;
+  int process_code, handle_code;
+
+  if (info->type == T_TCP_LISTEN) {
+    /* if this is a listen socket */
+    process_code = process_accept(info);
+    if (process_code == 0) {
+      /* complete */
+    } else if (process_code == 1) {
+      /* continue */
+      
+    } else {
+      /* error */
+      handle_code = info->error_handler(info->socket, 0 1); /* recreate a listen socket */
+      if (handle_code != 0) {
+        exit(1);                        /* MUST be replaced by a real exit_function */
+      }
+    }
+
+  } else if (info->type == T_TCP_SERVER_LV || info->type == T_TCP_CLIENT_LV) {
+    /* if this is a data socket(server or client */
+
+    if (info->state == S_IN_RECEIVE || info->state & S_IN_PROCESS) {
+      process_code = process_recv_lv_format(info);
+      if (process_code == 0) {
+        /* complete */
+        handle_code = info->message_handler(info->recv_buf, info->recv_buf_used);
+        reset_socket_info(info);
+        if (handle_code == 0) {
+          /* continue reading */
+        } else if (handle_code == -1) {
+          /* close socket */
+          destroy_socket_info(info);
+          ret = -1;
+        } else if (handle_code == 1) {
+          /* to send */
+          set_socket_state(info, S_IN_SEND);
+        } else if (handle_code == 2) {
+          /* in process */
+          set_socket_state(info, S_IN_PROCESS);
+        } else {
+          assert(0);
+        }
+
+      } else if (process_code == 1) {
+        /* continue */
+      } else if (process_code < 0) {
+        /* error */
+        destroy_socket_info(info);
+      } else {
+        assert(0);
+      }
+    } else if (info->state == S_IN_SEND) {
+      process_code = process_send(info);
+      if (process_code != 0) {
+        /* error */
+        handle_code = info->error_handler(info, 1);
+      }
+    } else {
+    }
+  } else if (info->type == T_HTTP_CLIENT || info->type == T_HTTP_SERVER) {
+    /* if this is an http data socket(server or client) */
+    if (info->state == S_IN_RECEIVE || info->state == S_IN_PROCESS) {
+      process_code = process_recv_http_format(info);
+      if (process_code == 0) {
+        /* complete */
+        handle_code = info->message_handler(info->socket, info->recv_buf, info->recv_used);
+        if (handle_code == 0) {
+          /* continue reading */
+        } else if (handle_code == 1) {
+          /* to send */
+        } else {
+          /* error */
+        }
+      } else if (process_code == 2) {
+        /* trunk complete */
+        /* do nothing */
+      } else if (process_code == 1) {
+        /* continue */
+      } else {
+        destroy_socket_info(info);
+      }
+    } else if (info->state == S_IN_SEND) {
+    }
+
+    } else if (process_code == 1) {
+      /* trunk complete */
+      /* do nothing */
+    } else
+  } else {
+  }
+}
+
+int process_connect(struct socket_info *info)
+{
+  assert(info);
+  assert(info->state == S_IN_CONNECT);
+  change_socket_state(S_IN_SEND);
+  return 0;
+}
+
+int process_error(struct socket_info *info)
+{
+  assert(info);
+  destroy_socket_info(info);
+  return 0;
+}
+
+int process_timer_event(struct timer *timer)
+{
+  assert(timer);
+  int ret = 1;
+  if (timer->timer_handler) {
+    ret = timer->timer_handler(timer->arg, timer->arglen);
+  }
+
+  return ret;
+}
+
