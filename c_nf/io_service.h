2511d97d508c0ca3aea13c111c44cf7d515cfb4c
ZHOU Xiaobo <zhouxiaobo@zhou.local>
2012-08-29 17:14:57 +0800

    3

--- c_nf/io_service.h
+++ c_nf/io_service.h
@@ -5,22 +5,19 @@ struct io_service {
   /* maintain a sorted list of server socket, used in finding idle socket */
   struct socket_info *server_list_head;
   struct socket_info *server_list_tail;
-  /* maintain a map, key is 2bytes from ip address
-   * here we use the lowwer 2 bytes of ip address
-   * because in a server-side situation the backend
-   * servers are in the same subnet such as 192.16.*.*
-   * when use 10.*.*.*, we should change the 'hash' method
-   */
-  struct socket_info *client_map[65535];
+
+  struct hash_table *client_table;
   struct timer *timer_list_head;
   uint64_t idle_timeout;
+  int *socket_vector;
 };
 
 
 void* get_io_service();
-int run();
+int run(void *io_serivce);
 
 int tcp_get_client(void *io_service, char *ipstr, uint32_t host_port, data_handler after_send_handler = 0);
+
 int tcp_send_to(void *io_service, int socket);
 int sync_tcp_send_and_recv(void *io_service, int socket);
 
