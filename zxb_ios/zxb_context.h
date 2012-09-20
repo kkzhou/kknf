e111d519c7ed8ad9450382cf1da426ac8a9d5638
zhouxiaobo <zhouxiaobo@dipperli-pc2.local>
2012-09-20 15:35:09 +0800

    4

--- zxb_ios/zxb_context.h
+++ zxb_ios/zxb_context.h
@@ -3,51 +3,52 @@
 
 #include <inttypes.h>
 
-typedef (int)(*zxb_context_callback)(struct zxb_context *ctx, void*);
+struct zxb_context;
+typedef int(*zxb_context_callback)(struct zxb_context *ctx, void*);
 
 enum zxb_context_type {
-  T_FILE = 1,
-  T_TCP,
-  T_UDP,
-  T_TIMER
+    T_FILE = 1,
+    T_TCP,
+    T_UDP,
+    T_TIMER
 };
 
 struct zxb_buf {
-  uint32_t net_ip;
-  uint16_t net_port;
-  char *buf;
-  uint32_t len;
-  struct zxb_buf *prev;
-  struct zxb_buf *next;
+    uint32_t net_ip;
+    uint16_t net_port;
+    char *buf;
+    uint32_t len;
+    struct zxb_buf *prev;
+    struct zxb_buf *next;
 };
 
 struct zxb_context {
-
-  enum zxb_context_type type;
-  int fd;                               /* >=0:file descriptor, <0: timer's id  */
-  uint32_t id;                          /* Unique id used by upper layer */
-
-  zxb_context_callback event_callback;
-  zxb_context_callback error_callback;
-
-  struct zxb_buf *write_buf;
-  struct zxb_buf *write_buf_tail;
-  uint32_t write_buf_num;
-  uint32_t begin_of_left_bytes;
-
-  char *read_buf;
-  uint32_t read_buf_len;
-  uint32_t read_buf_use;
-  uint32_t read_buf_len_min;
-  uint32_t read_buf_len_max;
-
-  /* for timer */
-  uint64_t fire_time_sec;
-  uint64_t fire_time_usec;
-  void *arg;
-
-  struct zxb_context *prev;
-  struct zxb_context *next;  
+    
+    enum zxb_context_type type;
+    int fd;                               /* >=0:file descriptor, <0: timer's id  */
+    uint32_t id;                          /* Unique id used by upper layer */
+    
+    zxb_context_callback event_callback;
+    zxb_context_callback error_callback;
+    
+    struct zxb_buf *write_buf;
+    struct zxb_buf *write_buf_tail;
+    uint32_t write_buf_num;
+    uint32_t begin_of_left_bytes;
+    
+    char *read_buf;
+    uint32_t read_buf_len;
+    uint32_t read_buf_use;
+    uint32_t read_buf_len_min;
+    uint32_t read_buf_len_max;
+    
+    /* for timer */
+    uint64_t fire_time_sec;
+    uint64_t fire_time_usec;
+    void *arg;
+    
+    struct zxb_context *prev;
+    struct zxb_context *next;  
 };
 
 
@@ -55,7 +56,7 @@ struct zxb_context {
 int zxb_context_create_tcp_listener(uint32_t net_ip, uint16_t net_port, int backlog);
 int zxb_context_create_udp_listener(uint32_t net_ip, uint16_t net_port);
 int zxb_context_connect(uint32_t net_to_ip, uint16_t net_to_port,
-                              uint32_t net_my_ip, uint16_t net_my_port);
+                        uint32_t net_my_ip, uint16_t net_my_port);
 int zxb_context_create_file(char *path);
 uint32_t zxb_context_ipstr_to_addr(char *ipstr);
 
--- zxb_ios/zxb_ios.c
+++ zxb_ios/zxb_ios.c
@@ -1,38 +1,41 @@
 
+#include <stdlib.h>
+#include <string.h>
 #include "zxb_ios.h"
 
 
 struct zxb_ios* zxb_ios_get()
 {
-  struct zxb_ios *ios = (struct zxb_ios*)malloc(sizeof (struct zxb_ios));
-  memset(ios, sizeof (struct zxb_ios), 0);
-
-  ios->ctx_fd_vector_size = 100000;
-  ios->ctx_fd_vector = (struct zxb_contex*)
-      malloc(sizeof (struct zxb_contex) * ios->ctx_fd_vector_size);
-  memset(ios->ctx_fd_vector, sizeof (struct zxb_contex) * ios->ctx_fd_vector_size);
-  
-  ios->ctx_id_vector_size = 1000;
-  ios->ctx_id_vector = (uint32_t*)malloc(sizeof (int) * ios->ctx_id_vector_size)
-  memset(ios->ctx_id_vector,sizeof (int) * ios->ctx_id_vector_size);
-
-  ios->fd_idle_timeout = 600;           /* destroy when idle for 600s */
-  return ios;
+    struct zxb_ios *ios = (struct zxb_ios*)malloc(sizeof (struct zxb_ios));
+    memset(ios, sizeof (struct zxb_ios), 0);
+    
+    ios->ctx_fd_vector_size = 100000;
+    ios->ctx_fd_vector = (struct zxb_contex*)malloc(sizeof (struct zxb_context) * ios->ctx_fd_vector_size);
+    memset(ios->ctx_fd_vector, sizeof (struct zxb_context) * ios->ctx_fd_vector_size, 0);
+    
+    ios->ctx_id_vector_size = 1000;
+    ios->ctx_id_vector = (uint32_t*)malloc(sizeof (int) * ios->ctx_id_vector_size)
+    memset(ios->ctx_id_vector,sizeof (int) * ios->ctx_id_vector_size, 0);
+    
+    ios->fd_idle_timeout = 600;           /* destroy when idle for 600s */
+    return ios;
 }
 
 int zxb_ios_tcp_listen_at(struct zxb_ios *ios, uint32_t net_ip, uint16_t net_port)
 {
-  int listen_socket = zxb_context_create_tcp_listener(net_ip, net_port, 1024);
-  if (listen_socket >= 0) {
+    int listen_socket = zxb_context_create_tcp_listener(net_ip, net_port, 1024);
+    if (listen_socket < 0) {
+        return -1;
+    }
+    struct zxb_context *new_ctx = zxb_context_create(listen_socket, 0, T_TCP);
     
-  }
 }
 
 int zxb_ios_udp_listen_at(struct zxb_ios *ios, uint32_t net_ip, uint16_t net_port);
 int zxb_ios_write(struct zxb_ios *ios, int fd, char *data, uint32_t len);
 int zxb_ios_udp_write(struct zxb_ios *ios, int fd, char *data, uint32_t len,
                       uint32_t net_ip, uint16_t net_port);
-int zxb_ios_set_recv_callback(struct zxb_ios *ios, int fd, zxb_contex_callback cb);
+int zxb_ios_set_recv_callback(struct zxb_ios *ios, int fd, zxb_context_callback cb);
 int zxb_ios_get_backend(struct zxb_ios *ios, uint32_t channel);
 uint32_t zxb_ios_add_timer(struct zxb_ios *ios, uint64_t sec_to_fire, uint64_t usec_to_fire, void *arg);
 int zxb_ios_cancel_timer(struct zxb_ios *ios, uint32_t id);
