b0fe1c4273f8f1a39123f43b4f674d062e360085
ZHOU Xiaobo <zhxb.ustc@gmail.com>
2013-01-28 18:56:17 +0800

    i

--- cppnf/async_tcp_socket.hpp
+++ cppnf/async_tcp_socket.hpp
@@ -54,21 +54,64 @@ namespace ZXBNF {
     class AsyncTCPDataSocket : public AsyncTCPSocket {
     protected:
 	AsyncTCPDataSocket(){};
-	~AsuncTCPDataSocket(){};
+	~AsuncTCPDataSocket(){ 
+	    while (msg_in_recv_) {
+		Buffer *buf = msg_in_recv_.next();
+		mempool()->ReturnBlock(msg_in_recv_);
+		msg_in_recv_ = buf;
+	    }
+	    while (send_buffer_list_) {
+		Buffer *buf = send_buffer_list_.next();
+		mempool()->ReturnBlock(send_buffer_list_);
+		send_buffer_list_ = buf;
+	    }
+	};
 	AsyncTCPDataSocket(int fd) { set_fd(fd); };
     public:
 	// event handler
 	// return value:
 	// 0: OK but not complete, so continue
 	// -1: error happens
-	// >0: OK and complete n bytes
-	int OnWritable();
+	// 1: OK and complete
+	int OnWritable() {
+	    struct iovec *v = new struct iovec[send_buffer_num_];
+	    Buffer *buf = send_buffer_head_;
+	    for (int i = 0; i < send_buffer_num_; i++) {
+		v[i].iov_base = buf->start() + buf->head();
+		v[i].iov_len = buf->tail() - buf->head();
+	    }
+	    assert(i == send_buffer_num_);
+	    int ret = writv(fd(), v, send_buffer_num_);
+	    if (ret < 0) {
+		delete v;
+		return -1;
+	    } else if (ret == 0) {
+		delete v;
+		return -2;
+	    } else {
+		
+	    }
+	};
 	int OnError();
 	int OnReadable();
     public:
 	// operations initiated by upper layer
-	int SendMessage(Buffer *msg, int msg_len) {
-	    
+	int SendMessage(Buffer *msg) {
+	    if (send_buffer_num_ == kMaxSendBufferNum) {
+		return -1;
+	    }
+	    if (send_buffer_tail_) {
+		send_buffer_tail_->next() = msg;
+		msg->next() = 0;
+		msg->prev() = send_buffer_tail_;
+		send_buffer_tail_ = msg;
+	    } else {
+		assert(send_buffer_head_ == 0);
+		assert(send_buffer_num_ == 0);
+		send_buffer_head_ = send_buffer_tail_ = msg;
+		msg->next() = msg->prev() = 0;
+	    }
+	    return ++send_buffer_num_;
 	};
 	int ConnectTo(Address &to, Addresss, &from);
 	int ConnectTo(Address &to);
@@ -80,7 +123,10 @@ namespace ZXBNF {
 	Buffer *msg_in_recv_;
 	int msg_size_;
 
-	Buffer *send_buffer_list_;
+	Buffer *send_buffer_head_;
+	Buffer *send_buffer_tail_;
+	int send_buffer_num_;
+	static const int kMaxSendBufferNum = 1000;
     };
 
 
